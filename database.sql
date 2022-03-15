39e1566f900c701451ab7322f5a36dbbf543ba7f
Brian Hvarregaard <brian@median.dk>
2022-03-15 17:33:08 +0100

    added sql script

---
+++ database.sql
@@ -0,0 +1,14 @@
+﻿--SQL script to create the Company table in the database. 
+--Please note that the data must exist. Default the name is "centisoft" this can be changed in the appsettings.json
+
+
+CREATE TABLE [dbo].[Company](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Street] [nvarchar](100) NOT NULL,
+	[City] [nvarchar](100) NOT NULL,
+	[ZipCode] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL
+) ON [PRIMARY]
+GO
+
